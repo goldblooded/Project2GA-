a5f7157c226ebaec2e7f604ecf33b756918236a4
Jason John <jkj1982@gmail.com>
2017-01-13 17:33:14 -0500

    First commit

---
+++ src/components/Exercise.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Exercises extends Component {
+  render() {
+    return (
+      <div className="Exercise">
+        Exercises
+      </div>
+    );
+  }
+}
+
+export default Exercises;
---
+++ src/components/Workout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Workout extends Component {
+  render() {
+    return (
+      <div className="Workout">
+        Workout
+      </div>
+    );
+  }
+}
+
+export default Workout;
